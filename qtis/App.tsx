14e758515703879348bc4a735af54749acfac569
Angela <angelapedreromoral@gmail.com>
2021-03-09 17:15:58 +0100

    Create navigation-tab, browser, profile

--- qtis/App.tsx
+++ qtis/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
-import { AppCover } from './src/screens'
+import { AppCover, SignIn, Register } from './src/screens'
 
 const Stack = createStackNavigator()
 
@@ -14,6 +14,8 @@ function App () {
       }}
       initialRouteName={AppCover}>
         <Stack.Screen name="AppCover" component={AppCover}/>
+        <Stack.Screen name="SignIn" component={SignIn}/>
+        <Stack.Screen name="Register" component={Register}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
--- qtis/src/screens/index.tsx
+++ qtis/src/screens/index.tsx
@@ -1,9 +1,13 @@
 import AppCover from './appCover'
 import SignIn from './signIn'
 import Register from './register'
+import Browser from './browser'
+import Profile from './profile'
 
 export {
   AppCover,
   SignIn,
-  Register
+  Register,
+  Profile,
+  Browser
 }
