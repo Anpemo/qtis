c46b28d4d33b89cef81141682d72027353f06a86
Angela <angelapedreromoral@gmail.com>
2021-03-09 15:24:48 +0100

    Take return content to a new component and stat navigation

--- qtis/App.tsx
+++ qtis/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0000',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  text: {
-    color: '#12d4ef',
-    fontSize: 24
-  }
-})
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { appCover } from './src/screens'
+const Stack = createStackNavigator()
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+      </Stack.Navigator>
+      </NavigationContainer>
+
   )
 }
