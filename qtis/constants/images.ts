0bfdd9e0ea1bf576e4017452faf856c1f171ce9d
Angela <angelapedreromoral@gmail.com>
2021-03-09 15:41:54 +0100

    Move constants folder outside from assetss

---
+++ qtis/constants/images.ts
@@ -0,0 +1,25 @@
+const coverGirl = require('../assets/images/coverGirl')
+const oldLady = require('../assets/images/oldLady')
+const profileHeader = require('../assets/images/profileHeader')
+const backgroundReview = require('../assets/images/backgroundReview')
+const detailBackground = require('../assets/imageS/detailBackground')
+const oilCategory = require('../assets/images/manos/aceite')
+const creamsCategory = require('../assets/images/manos/creams')
+const masksCategory = require('../assets/images/manos/masks')
+const shampooCategory = require('../assets/images/manos/shampoo')
+const soapsCategory = require('../assets/images/manos/soaps')
+const toolsCategory = require('../assets/images/manos/tools')
+
+export default {
+  coverGirl,
+  oldLady,
+  profileHeader,
+  backgroundReview,
+  detailBackground,
+  oilCategory,
+  creamsCategory,
+  masksCategory,
+  shampooCategory,
+  soapsCategory,
+  toolsCategory
+}
---
+++ qtis/constants/theme.ts
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native'
+const { width, height } = Dimensions.get('window')
+
+export const COLORS = {
+  // base colors
+  cream: '#DDA15E',
+  brown: '#D4A373',
+  backgroundGrey: '#C0BAB3',
+  lettersGrey: '#000000',
+
+  // colors
+  black: '#141414',
+  white: '#FFFFFF'
+}
