fdbfa2965c2b3c7d36898ba8910931c25dfd3b18
Angela <angelapedreromoral@gmail.com>
2021-03-09 16:19:53 +0100

    Download Montserrat font family & add FONTS and SIZE to theme

--- qtis/constants/theme.ts
+++ qtis/constants/theme.ts
@@ -12,3 +12,30 @@ export const COLORS = {
   black: '#141414',
   white: '#FFFFFF'
 }
+
+export const SIZES = {
+  // global sizes
+  buttonheight: 38,
+  buttonRadius: 50,
+  squareRadius: 8,
+  squareImageRadius: 5,
+
+  // font sizes
+  buttonText: 18,
+  h2: 30,
+  h3: 18,
+  p20: 20,
+  p16: 16,
+  p14: 14,
+  p13: 13,
+
+  // app dimensions
+  width,
+  height
+}
+
+export const FONTS = {
+  buttons: { fontFamily: 'Montserrat-Regular', fontSize: SIZES.buttonText, lineHeight: 22, letterSpacing: 3 },
+  h2: { fontFamily: 'Montserrat-Regular', fontSize: SIZES.h2, lineHeight: 37, letterSpacing: 3 },
+  h3: { fontFamily: 'Montserrat-Regular', fontSize: SIZES.h3, lineHeight: 22, letterSpacing: 3 }
+}
