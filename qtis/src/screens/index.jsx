818f5d43721a39142b5109743d34dc8ce57774bf
Angela <angelapedreromoral@gmail.com>
2021-03-09 16:35:44 +0100

    Build a basic structure for our screens

---
+++ qtis/src/screens/index.jsx
@@ -0,0 +1,9 @@
+import AppCover from './appCover'
+import SignIn from './signIn'
+import Register from './register'
+
+export {
+  AppCover,
+  SignIn,
+  Register
+}
--- qtis/src/screens/signIn.tsx
+++ qtis/src/screens/signIn.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function AppCover () {
+export default function SignIn () {
   return (
     <View >
       <Text>Hello World!</Text>
