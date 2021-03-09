0aea567873bf3ff7d25362fdabf9b431e51d6451
Angela <angelapedreromoral@gmail.com>
2021-03-09 09:02:35 +0100

    Add product and review routes at index

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,6 +4,8 @@ const debug = require('debug')('app');
 const { connect } = require('mongoose');
 require('dotenv').config();
 const userRouter = require('./src/routes/userRoutes');
+const productRouter = require('./src/routes/productRoutes');
+const reviewRouter = require('./src/routes/reviewRoutes');
 
 const app = express();
 const port = process.env.PORT;
@@ -16,5 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/user', userRouter);
+app.use('/product', productRouter);
+app.use('/review', reviewRouter);
 
 app.listen(port, () => debug(`Esto parece que funciona en el puerto ${port} `));
