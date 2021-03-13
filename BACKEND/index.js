6b28193815be8c10dd4e83bc96568a66041119d6
Angela <angelapedreromoral@gmail.com>
2021-03-13 17:17:35 +0100

    Add route to index.js on  backend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('app');
+const cors = require('cors');
 const { connect } = require('mongoose');
+const session = require('express-session');
 require('dotenv').config();
 
 const userRouter = require('./src/routes/userRoutes');
 const productRouter = require('./src/routes/productRoutes');
 const reviewRouter = require('./src/routes/reviewRoutes');
+const authRouter = require('./src/routes/authRouter');
 
 const app = express();
 const port = process.env.PORT;
@@ -14,13 +17,18 @@ const DDBB = process.env.DDBB_URL;
 
 connect(DDBB, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
+
+app.use(session({ secret: 'skylab directory' }));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(session({ secret: '' }));
+
+require('./src/passport')(app);
 
 app.use('/user', userRouter);
 app.use('/product', productRouter);
 app.use('/review', reviewRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, () => debug(`Esto parece que funciona en el puerto ${port} `));
--- BACKEND/src/passport/strategies/local.strategy.js
+++ BACKEND/src/passport/strategies/local.strategy.js
@@ -26,7 +26,6 @@ function LocalStrategy() {
         })();
       }
     )
-
   );
 }
 
